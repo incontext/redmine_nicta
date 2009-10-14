92a10491b8565fdcb357cb14a0353ba54b7477a9
jackhong <jackhongx@gmail.com>
2009-10-14 14:26:13 +1100

    Populate custom field values for script and script run

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -8,30 +8,53 @@ class ResultsController < ApplicationController
   def create
     doc, items = Hpricot::XML(request.raw_post), []
     outcome = (doc/"experiment-outcome").first
+    outcome_script = (doc/"experiment-outcome"/"script").first
+    outcome_measurments = (doc/"experiment-outcome"/"measurements").first
 
     project = Project.find_by_identifier(outcome.at('project').innerHTML)
     user = User.find_by_login(outcome.at('userid').innerHTML)
-    script = Issue.find_by_subject(outcome.at('id').innerHTML) || Issue.new
+    script = Issue.find_by_subject(project.identifier + '_' + outcome.at('id').innerHTML) || Issue.new
 
-    if script.new_record?
-      script.project = project
-      script.author = user
-      script.subject = project.identifier + '_' + outcome.at('id').innerHTML
-      script.tracker = Tracker.find_by_name('Script')
-      #script.save!
-    end
+    ActiveRecord::Base.transaction do
+      begin
+        if script.new_record?
+          script.project = project
+          script.author = user
+          script.subject = project.identifier + '_' + outcome.at('id').innerHTML
+          script.tracker = Tracker.find_by_name('Script')
+          field_values = [['Type', 'Wifi'], ['Batch', '1'], ['Resume', '1']].inject ({}) do |hash, field_value|
+            field = IssueCustomField.find_by_name(field_value[0])
+            hash[field.id] = field_value[1] if field
+            hash
+          end
+          script.custom_field_values = field_values
+          script.save!
+        end
 
-    #script_run = Issue.new
-    #script_run.project = project
-    #script_run.author = user
-    #script_run.subject = project.identifier + '_' + outcome.at('id').innerHTML
-    #script_run.tracker = Tracker.find_by_name('Script')
-    #script_run.save!
-
-    if script.save!
-      render :xml => {:notice => 'Added experiment outcome'}, :status => 201
-    else
-      render :xml => {:error => 'Failed to add experiment outcome' + issue.errors.to_s}
+        script_run = Issue.new
+        script_run.tracker = Tracker.find_by_name('Script run')
+        script_run.project = project
+        script_run.author = user
+        script_run.subject = 'Experiment outcome for ' + script.subject
+        field_values = [['Script version', outcome_script.at('revision').innerHTML],
+         ['Script uri', outcome_script.at('uri').innerHTML],
+         ['Log file', outcome_measurments.at('log').innerHTML],
+         ['Log data', outcome_measurments.at('logdata').innerHTML],
+         ['Run start time', Time.parse(outcome.at('start').innerHTML).strftime('%I:%M:%S %p')],
+         ['Run duration', outcome.at('duration').innerHTML],
+         ['Measurements DB', outcome_measurments.at('database').innerHTML],
+         ['Measurements metadata', outcome_measurments.at('metadata').innerHTML]].inject ({}) do |hash, field_value|
+          field = IssueCustomField.find_by_name(field_value[0])
+          hash[field.id] = field_value[1] if field
+          hash
+        end
+        script_run.custom_field_values = field_values
+        script_run.save!
+        script_run.move_to_child_of(script)
+        render :xml => {:notice => 'Added experiment outcome'}, :status => 201
+      rescue
+        render :xml => {:error => 'Failed to add experiment outcome' + e.backtrace.join(' ')}, :status => 500
+      end
     end
   end
 
--- db/migrate/20091013045808_init_trackers_and_custom_fields.rb
+++ db/migrate/20091013045808_init_trackers_and_custom_fields.rb
@@ -1,11 +1,12 @@
 class InitTrackersAndCustomFields < ActiveRecord::Migration
   def self.up
-    Issue.destroy_all
+    CustomValue.destroy_all
+    Issue.delete_all
     Tracker.destroy_all
     script_tracker = Tracker.create!(:name => 'Script', :is_in_chlog => true, :is_in_roadmap => true)
     script_run_tracker = Tracker.create!(:name => 'Script run', :is_in_chlog => true, :is_in_roadmap => true)
 
-    IssueCustomField.destroy_all
+    CustomField.destroy_all
     #Script
     IssueCustomField.create!(:name => 'Type', :field_format => 'list', :possible_values => ['Wifi', 'Ethernet', '3G', 'DSRC'],
                              :is_required => true, :is_for_all => true, :is_filter => true).trackers << script_tracker
@@ -17,11 +18,13 @@ class InitTrackersAndCustomFields < ActiveRecord::Migration
     #Script run
     IssueCustomField.create!(:name => 'Script version', :field_format => 'text', :is_for_all => true, :searchable => true).trackers << script_run_tracker
     IssueCustomField.create!(:name => 'Script uri', :field_format => 'text', :is_for_all => true, :searchable => true).trackers << script_run_tracker
-    IssueCustomField.create!(:name => 'Run start time', :field_format => 'date', :is_for_all => true).trackers << script_run_tracker
-    IssueCustomField.create!(:name => 'Run duration', :field_format => 'text', :is_for_all => true).trackers << script_run_tracker
+    IssueCustomField.create!(:name => 'Run start time', :field_format => 'string', :regexp =>  "^((([0]?[1-9]|1[0-2])(:|\.)[0-5][0-9]((:|\.)[0-5][0-9])?( )?(AM|am|aM|Am|PM|pm|pM|Pm))|(([0]?[0-9]|1[0-9]|2[0-3])(:|\.)[0-5][0-9]((:|\.)[0-5][0-9])?))$", :is_for_all => true).trackers << script_run_tracker
+    IssueCustomField.create!(:name => 'Run duration', :field_format => 'string', :is_for_all => true).trackers << script_run_tracker
     IssueCustomField.create!(:name => 'Attribute text', :field_format => 'text', :is_for_all => true, :searchable => true).trackers << script_run_tracker
     IssueCustomField.create!(:name => 'Log file', :field_format => 'text', :is_for_all => true).trackers << script_run_tracker
-    IssueCustomField.create!(:name => 'Run results', :field_format => 'text', :is_for_all => true).trackers << script_run_tracker
+    IssueCustomField.create!(:name => 'Log data', :field_format => 'text', :is_for_all => true).trackers << script_run_tracker
+    IssueCustomField.create!(:name => 'Measurements DB', :field_format => 'text', :is_for_all => true).trackers << script_run_tracker
+    IssueCustomField.create!(:name => 'Measurements metadata', :field_format => 'text', :is_for_all => true).trackers << script_run_tracker
   end
 
   def self.down
