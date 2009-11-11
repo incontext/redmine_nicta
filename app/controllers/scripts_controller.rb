c51e622fef3bce28ee66d2a1e0673ea457215489
Jack Hong <jackhongx@gmail.com>
2009-11-11 12:21:36 +1100

    Redirect to 404 when project not found in scripts page

--- app/controllers/scripts_controller.rb
+++ app/controllers/scripts_controller.rb
@@ -50,5 +50,7 @@ class ScriptsController < ApplicationController
 
   def find_project
     @project = Project.find(params[:project_id])
+  rescue ActiveRecord::RecordNotFound
+    render_404
   end
 end
