1228ddb583844a9a83be0f915d00dc7c3d019960
Jack Hong <jackhongx@gmail.com>
2010-07-08 15:14:21 +1000

    WIP MyPage view test

--- lib/issue_model_patch.rb
+++ lib/issue_model_patch.rb
@@ -11,6 +11,7 @@ module IssueModelPatch
       validates_uniqueness_of :identifier, :scope => :project_id unless 'identifier.nil?'
       validates_format_of :identifier, :with => /^[A-Za-z0-9]+$/ unless 'identifier.nil?'
       after_create :assign_identifier_and_script_path
+      after_create :create_event
     end
 
   end
@@ -25,6 +26,10 @@ module IssueModelPatch
       self.identifier ||= "script#{id}"
       self.script_path ||= "script#{id}.rb"
       self.save!
+    end
+
+    def create_event
+      Event.create!(:name => subject, :start_at => Time.zone.now, :end_at => 2.hours.from_now)
     end
   end
 end
