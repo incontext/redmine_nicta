937455f3d958961fa926e531b3b2375479b3d440
Jack Hong <jackhongx@gmail.com>
2010-08-24 23:28:14 +1000

    Support multiple nodes in result graph

--- lib/experiment_issue_hook.rb
+++ lib/experiment_issue_hook.rb
@@ -132,46 +132,51 @@ class ExperimentIssueHook < Redmine::Hook::ViewListener
 
   def render_graph(result_filename)
     db = SQLite3::Database.new(result_filename)
-    db.results_as_hash = true
-    tables = db.execute( "select name from sqlite_master where type = 'table'" ).map {|v| v['name']}.flatten.find_all {|v| !(v =~ /^_/)}
+    db.results_as_hash = false
+    tables = db.execute( "select name from sqlite_master where type = 'table'" ).flatten.find_all {|v| !(v =~ /^_/)}
     tables.map do |table|
-      rows = db.execute( "select * from #{table} join _senders where oml_sender_id = _senders.id")
-      graph_rows = rows.map do |row|
-        {}.tap do |hash|
-          row.keys.each do |key|
-            if !(key =~ /^oml/ || key =~ /id$/ || key.class == Fixnum) && row[key] =~ /^(-|\.|[0-9])+$/
-              hash[key] = row[key]
+      db.results_as_hash = false
+      nodes = db.execute( "select distinct name from #{table} join _senders on oml_sender_id = _senders.id")
+      nodes.map do |node|
+        db.results_as_hash = true
+        rows = db.execute( "select * from #{table} join _senders on oml_sender_id = _senders.id and name = '#{node}'")
+        graph_rows = rows.map do |row|
+          {}.tap do |hash|
+            row.keys.each do |key|
+              if !(key =~ /^oml/ || key =~ /id$/ || key.class == Fixnum) && row[key] =~ /^(-|\.|[0-9])+$/
+                hash[key] = row[key]
+              end
             end
+            hash['oml_ts_server'] = row['oml_ts_server']
           end
-          hash['oml_ts_server'] = row['oml_ts_server']
         end
+        build_graph(table, node, graph_rows) unless graph_rows.empty? || graph_rows.first.keys.size < 2
       end
-      build_graph(table, graph_rows) unless graph_rows.empty? || graph_rows.first.keys.size < 2
-    end.join("")
+    end.flatten.join("")
   end
 
-  def build_graph(table, rows)
+  def build_graph(table, node, rows)
     "
     <hr />
-    <p><b>Graph #{table}</b></p>
-    <div class=\"graph\" id=\"graph_#{table}\" style=\"width: 550px; height: 350px;\"></div>
+    <p><b>Graph #{table} : #{node} </b></p>
+    <div class=\"graph\" id=\"graph_#{table}_#{node}\" style=\"width: 550px; height: 350px;\"></div>
     <script type=\"text/javascript\">
-      g = new Dygraph(
-        document.getElementById(\"graph_#{table}\"),
+      g_#{table}_#{node}= new Dygraph(
+        document.getElementById(\"graph_#{table}_#{node}\"),
     " +
     "\"oml_ts_server,#{(rows.first.keys-['oml_ts_server']).join(',')}\\n\" + " +
     rows.map { |row| "\"#{([row['oml_ts_server']] + row.except('oml_ts_server').values).join(',')}\\n\"" }.join(' + ') +
     "
       );
-      function stockchange(el) {
-        g.setVisibility(el.id, el.checked);
+      function data_#{table}_#{node}_change(el) {
+        g_#{table}_#{node}.setVisibility(el.id, el.checked);
       }
     </script>
     <p>Display:
     " +
     (rows.first.keys-['oml_ts_server']).each_with_index.map do |key, index|
       "
-      <input type=\"checkbox\" checked=\"\" onclick=\"stockchange(this)\" id=\"#{index}\">
+      <input type=\"checkbox\" checked=\"\" onclick=\"data_#{table}_#{node}_change(this)\" id=\"#{index}\">
       <label for=\"#{index}\"> #{key} </label>
       "
     end.join("") +
